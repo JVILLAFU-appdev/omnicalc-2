866717e06bb554db0e93772affb73949205d2ebc
JVILLAFU <JVILLAFU@CHICAGOBOOTH.EDU>
2023-02-22 04:18:29 +0000

    Omnicalc 2_v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
 
+  def home
+    render({ :template => "omnicalc/home.html.erb"})
+  end
+
+  def add
+    render({ :template => "omnicalc/add.html.erb"})
+  end
+
+  def subtract
+    render({ :template => "omnicalc/subtract.html.erb"})
+  end
+
+  def multiply
+    render({ :template => "omnicalc/multiply.html.erb"})
+  end
+
+  def divide
+    render({ :template => "omnicalc/divide.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get("/", { :controller => "application", :action => "home" })
+get("/wizard_add", { :controller => "application", :action => "add" })
+get("/wizard_subtract", { :controller => "application", :action => "subtract" })
+get("/wizard_multiply", { :controller => "application", :action => "multiply" })
+get("/wizard_divide", { :controller => "application", :action => "divide" })
+
 end
