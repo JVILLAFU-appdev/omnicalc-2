e0b5e11acf5cb54e799bac0aca85de5d93ea12fa
JVILLAFU <JVILLAFU@CHICAGOBOOTH.EDU>
2023-02-22 05:08:41 +0000

    Omnicalc 2 Part II

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,38 @@ class ApplicationController < ActionController::Base
   end
 
   def add
+
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @sum = @first_num + @second_num
+
     render({ :template => "omnicalc/add.html.erb"})
   end
 
   def subtract
+
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @subtract = @first_num - @second_num
+
     render({ :template => "omnicalc/subtract.html.erb"})
   end
 
   def multiply
+
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @multiply = @first_num * @second_num
+
     render({ :template => "omnicalc/multiply.html.erb"})
   end
 
   def divide
+
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @divide = @first_num / @second_num
+    
     render({ :template => "omnicalc/divide.html.erb"})
   end
 
